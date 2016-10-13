3b5fbc7b3fc95496f369f1db129133bf7a29bb3f
Darren Green <darrensgreen@gmail.com>
2016-10-12 21:11:13 -0500

    Refomatted
    Refomatted

--- src/postSelector/actions.js
+++ src/postSelector/actions.js
@@ -1,8 +1,3 @@
-import * as C from './constants'
 import { Actions } from '../subreddit'
 
-export const updateSelected = selected => {
-    return (dispatch) => {
-        dispatch(Actions.fetchPosts(selected))
-    }
-}
+export const updateSelected = selected => (dispatch) => dispatch(Actions.fetchPosts(selected))
