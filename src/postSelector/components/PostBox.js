9aff6788104949dcc561d0636bd75c65aeae379d
Darren Green <darren@gruen.site>
2016-10-13 11:26:08 -0500

    Removed the DebounceInput
    Removed the wrapper component to do some more research

--- src/postSelector/components/PostBox.js
+++ src/postSelector/components/PostBox.js
@@ -8,9 +8,7 @@ const postBox = props => <div className="row">
     <div className="col-xs-12 col-md-6 col-lg-3">
         <div className="form-group">
             <label htmlFor="post_select">Select sub-reddit</label>
-            <DebounceInput
-                minLength={ 2 }
-                debounceTimeout={ 1000 }
+            <input
                 className="form-control"
                 onChange={ (c) => props.store.dispatch(Actions.updateSelected(c.target.value)) }
                 type="text"
