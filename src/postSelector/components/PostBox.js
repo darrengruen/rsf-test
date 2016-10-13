3d1676712a0d2cc4d49510ef7cbbed35b011dcb9
Darren Green <darren@gruen.site>
2016-10-13 11:31:17 -0500

    Updated with some comments
    A couple of notes on implementation

--- src/postSelector/components/PostBox.js
+++ src/postSelector/components/PostBox.js
@@ -2,15 +2,22 @@ import debounce from 'lodash/debounce'
 import React, { PropTypes } from 'react'
 import * as Actions from '../actions'
 import throttle from 'lodash/throttle'
+// Take a look at https://github.com/nkbt/react-debounce-input/blob/master/src/Component.js
 import DebounceInput from 'react-debounce-input'
 
+// The debounce is set high for testing. Real world would be more like 150
 const postBox = props => <div className="row">
     <div className="col-xs-12 col-md-6 col-lg-3">
         <div className="form-group">
             <label htmlFor="post_select">Select sub-reddit</label>
-            <input
+            <DebounceInput
+                minLength={ 2 }
+                debounceTimeout={ 1000 }
                 className="form-control"
-                onChange={ (c) => props.store.dispatch(Actions.updateSelected(c.target.value)) }
+                onChange={ (c) => {
+                    c.persist()
+                    throttle(props.store.dispatch(Actions.updateSelected(c.target.value)), 500)
+                } }
                 type="text"
                 value={ props.postSelect } id="post_select"
             />
