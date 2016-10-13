bdfd1404520c135a4a3bbfe77120e9cb5ec7bff3
Darren Green <darren@gruen.site>
2016-10-12 21:52:49 -0500

    Added a debounce component
    Added a simple debounce component. Works pretty nicely

--- src/postSelector/components/PostBox.js
+++ src/postSelector/components/PostBox.js
@@ -1,11 +1,16 @@
+import debounce from 'lodash/debounce'
 import React, { PropTypes } from 'react'
 import * as Actions from '../actions'
+import throttle from 'lodash/throttle'
+import DebounceInput from 'react-debounce-input'
 
 const postBox = props => <div className="row">
     <div className="col-xs-12 col-md-6 col-lg-3">
         <div className="form-group">
             <label htmlFor="post_select">Select sub-reddit</label>
-            <input
+            <DebounceInput
+                minLength={ 2 }
+                debounceTimeout={ 1000 }
                 className="form-control"
                 onChange={ (c) => props.store.dispatch(Actions.updateSelected(c.target.value)) }
                 type="text"
