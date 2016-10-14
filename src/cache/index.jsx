cc092732fa0ff106b306dfbe8fffa835fe17f17f
Darren Green <darren@gruen.site>
2016-10-13 21:11:49 -0500

    cache index
    cache index

---
+++ src/cache/index.jsx
@@ -0,0 +1,9 @@
+import * as acts from './actions'
+import * as cons from './constants'
+import red from './reducers'
+
+export const actions = acts
+export const constants = cons
+export const reducers = red
+
+export default { actions, constants, reducers }
