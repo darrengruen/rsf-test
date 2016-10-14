9625c4b46ea8fe7eaf1b5085226908d23d6a4aa0
Darren Green <darren@gruen.site>
2016-10-13 21:13:22 -0500

    Refactor and update to show loading
    Refactor and update to show loading

--- src/subreddit/reducers.js
+++ src/subreddit/reducers.js
@@ -3,16 +3,20 @@ import { constants as PS } from '../postSelector'
 
 const srReducer = (state = {
     isLoading: false,
-    selected: '',
     items: [],
+    selected: '',
 }, action) => {
     switch (action.type) {
     case PS.UPDATE:
         return Object.assign({}, state, { selected: action.selected })
     case C.REQUEST:
-        return Object.assign({}, state, { items:action.subreddits, selected: action.subreddit })
+        return Object.assign({}, state, {
+            isLoading: true,
+            items:action.subreddits,
+            selected: action.subreddit
+        })
     case C.RECEIVE:
-        return Object.assign({}, state, { items: action.items, selected: action.selected })
+        return Object.assign({}, state, { isLoading: false, items: action.items, selected: action.selected })
     default:
         return state
     }
