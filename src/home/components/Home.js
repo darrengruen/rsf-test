ab0c3df4d3e165d8f01b54ff727b6a8c888a3034
Darren Green <darren@gruen.site>
2016-10-13 21:12:16 -0500

    Added a "loading" message
    Added a "loading" message

--- src/home/components/Home.js
+++ src/home/components/Home.js
@@ -6,6 +6,7 @@ import { SubReddit } from '../../subreddit'
 const home = props => <div className="container-fluid">
     <h1>Real SoftWorks</h1>
     <PostBox { ...props } />
+    { props.state.subreddit.isLoading && <h2 className="text-center">Loading</h2>}
     {
             props.state.subreddit.items ?
             map(props.state.subreddit.items, (sr, ind) => <SubReddit subreddit={ sr } key={ ind } /> ) :
